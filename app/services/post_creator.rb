ecca7c0c4fb92f8585f3094c5e82dc6fa13fe4c0
Marcin Bunsch <marcin@futuresimple.com>
2014-10-15 07:39:58 +0200

    Moving tweeting and indexing to submethods in PostCreator

--- app/services/post_creator.rb
+++ app/services/post_creator.rb
@@ -1,17 +1,26 @@
 class PostCreator < MethodStruct.new(:current_user, :post_params, :method_name => :create)
 
   def create
-    @post = Post.new(post_params)
+    post = Post.new(post_params)
 
-    if @post.save
-      twitter = TwitterClient.new(current_user)
-      twitter.tweet "Blogged: #{@post.title}"
-
-      search = SearchClient.new
-      search.index @post
+    if post.save
+      publish_to_twitter(post)
+      index_post(post)
     end
 
-    @post
+    post
+  end
+
+  private
+
+  def publish_to_twitter(post)
+    twitter = TwitterClient.new(current_user)
+    twitter.tweet "Blogged: #{post.title}"
+  end
+
+  def index_post(post)
+    search = SearchClient.new
+    search.index post
   end
 
 end
